08fd5f8a5d3e7dc3794e9bb190b5749c15e270ff
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-12-05 11:34:22 -0800

    handle divisional (not pi) users; add batchit permissions

--- config.py
+++ config.py
@@ -27,6 +27,9 @@ BUCKET_ACCESS_POLICY = "arn:aws:iam::{account}:policy/fh-pi{pi}-bucket-access"
 END_USER_BATCH_GROUP = "fh-pi-end-user-batch-access"
 PASS_ROLE_POLICY = "arn:aws:iam::{}:policy/fh_allow_passrole".format(ACCOUNT_NUMBER)
 
+BATCHIT_POLICY = "arn:aws:iam::{}:policy/batchit-instance-permissions".format(ACCOUNT_NUMBER)
+
+
 if ACCOUNT_NUMBER == SCICOMP_ACCOUNT_NUMBER:
     BUCKET_PREFIX = "fh-pi-sc-"
 elif ACCOUNT_NUMBER == HSE_ACCOUNT_NUMBER:
--- onboarding.py
+++ onboarding.py
@@ -19,7 +19,7 @@ import config
 
 
 
-def main():
+def main(): # pylint: disable=too-many-locals
     "Do the work."
     if not len(sys.argv) == 3:
         print("usage: {} hutchnet-id-of-user pi_lastname_firstinitial".format(sys.argv[0]))
@@ -36,15 +36,29 @@ def main():
     iam = boto3.client("iam")
 
     # check to see if PI group exists
-    pi_group_name = "fi-pi-{}".format(pi)
+    pi_group_name = "fh-pi-{}".format(pi)
     try:
         iam.get_group(GroupName=pi_group_name)
     except iam.exceptions.NoSuchEntityException:
-        print("There is no IAM group {} for this PI!".format(pi_group_name))
+        div_group_name = pi_group_name.replace("fh-pi", "fh-div")
+        try:
+            iam.get_group(GroupName=div_group_name)
+            print("This is a divisional group (not a PI group).")
+            pi_group_name = div_group_name
+        except iam.exceptions.NoSuchEntityException:
+            print("There is no IAM group {} for this PI!".format(pi_group_name))
+            sys.exit(1)
+
+    if pi_group_name.startswith("fh-pi"):
+        type_of_group = "pi"
+    elif pi_group_name.startswith("fh-div"):
+        type_of_group = "div"
+    else:
+        print("Unknown group type!")
         sys.exit(1)
 
     # check to see if pi batch task role exists
-    pi_batch_role_name = "fh-pi-{}-batchtask".format(pi)
+    pi_batch_role_name = "fh-{}-{}-batchtask".format(type_of_group, pi)
     assume_role_document = {'Statement': [{'Action': 'sts:AssumeRole',
                                            'Effect': 'Allow',
                                            'Principal': {'Service': 'ecs-tasks.amazonaws.com'},
@@ -61,16 +75,18 @@ def main():
                         Description="batch role granting S3 access to {} bucket"\
                                             .format(pi))
         # attach the bucket policy to it
-        bucket_policy_name = "fh-pi-{}-bucket-access".format(pi)
+        bucket_policy_name = "fh-{}-{}-bucket-access".format(type_of_group, pi)
         bucket_policy_arn = "arn:aws:iam::{}:policy/{}".format(config.ACCOUNT_NUMBER,
                                                                bucket_policy_name)
         iam.attach_role_policy(RoleName=pi_batch_role_name, PolicyArn=bucket_policy_arn)
+        iam.attach_role_policy(RoleName=pi_batch_role_name, PolicyArn=config.BATCHIT_POLICY)
+
     # add user to end user batch group
     iam.add_user_to_group(GroupName=config.END_USER_BATCH_GROUP, UserName=user)
     # attach pass-role policy to user
     iam.attach_user_policy(UserName=user, PolicyArn=config.PASS_ROLE_POLICY)
     # create an fh-pi-pinam-e-passrole policy
-    pi_passrole_policy_name = "fh-pi-{}-passrole".format(pi)
+    pi_passrole_policy_name = "fh-{}-{}-passrole".format(type_of_group, pi)
     pi_batch_role_arn = "arn:aws:iam::{}:role/{}".format(config.ACCOUNT_NUMBER,
                                                          pi_batch_role_name)
     policy = dict(Version="2012-10-17",
